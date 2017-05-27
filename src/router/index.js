37a7acc2ce52d5aaea0d716d07bce15cd755223a
mac <2675601413@qq.com>
2017-05-27 15:30:23 +0800

    在个人中心页面中,点设置跳到setting页面，进行详细的设置。设置页面中写了编辑用户信息页，账号密码页。

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import index from '@/components/user.vue'
+import index from '@/components/change_user.vue'
 import setting from '@/components/setting.vue'
 Vue.use(Router)
 
