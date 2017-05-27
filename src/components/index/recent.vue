c78e08f239094729c960d312e4b380d64138213f
你看我名字帅气么 <676962537@qq.com>
2017-05-27 19:54:36 +0800

    马赫男

--- src/components/index/recent.vue
+++ src/components/index/recent.vue
@@ -4,52 +4,77 @@
       <div class="row list1">
         最近访问
       </div>
-      <div class="row list2">
-        <div class="col-xs-6">
-        <span class="glyphicon glyphicon-qrcode" aria-hidden="true"
-              style="font-size: 35px;">
-
-        </span>
-          <div>景点美食</div>
-        </div>
-        <div class="col-xs-6">
-        <span class="glyphicon glyphicon-qrcode" aria-hidden="true"
-              style="font-size: 35px;">
-
-        </span>
-          <div>商城</div>
-        </div>
+      <div class="row list1">
+        东京
       </div>
       <div class="row list3">
-        <div class="col-xs-6">
-        <span class="glyphicon glyphicon-qrcode" aria-hidden="true"
+        <div class="col-xs-3">
+        <span class="glyphicon glyphicon-cutlery" aria-hidden="true"
               style="font-size: 35px;">
 
         </span>
-          <div>景点美食</div>
-        </div>
-        <div class="col-xs-6">
-        <span class="glyphicon glyphicon-qrcode" aria-hidden="true"
+        <div>景点美食</div>
+      </div>
+
+        <div class="col-xs-3">
+        <span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"
               style="font-size: 35px;">
 
         </span>
           <div>商城</div>
         </div>
+        <div class="col-xs-3">
+        <span class="glyphicon glyphicon-folder-close" aria-hidden="true"
+              style="font-size: 35px;">
+
+        </span>
+          <div>锦囊</div>
+        </div>
+
+        <div class="col-xs-3">
+        <span class="glyphicon glyphicon-globe" aria-hidden="true"
+              style="font-size: 35px;">
+
+        </span>
+          <div>地图</div>
+        </div>
       </div>
     </div>
-    <div class="container-fluid recent2">
-      <div class="row name">
-        <div class="col-xs-4">东京</div>
-        <div class="col-xs-4">佛罗伦萨</div>
-        <div class="col-xs-4">巴厘岛</div>
+
+
+    <div class="row good">
+      <div class="col-xs-4">
+        <goods>
+          <span class="glyphicon glyphicon-thumbs-up" aria-hidden="true" style="font-size: 25px;" slot="icon"></span>
+          <span slot="inner">佛罗伦萨</span>
+        </goods>
+      </div>
+      <div class="col-xs-4">
+        <goods>
+          <span class="glyphicon glyphicon-thumbs-up" aria-hidden="true" style="font-size: 25px;" slot="icon"></span>
+          <span slot="inner">巴厘岛</span>
+        </goods>
+      </div>
+      <div class="col-xs-4">
+        <goods>
+          <span class="glyphicon glyphicon-thumbs-up" aria-hidden="true" style="font-size: 25px;" slot="icon"></span>
+          <span slot="inner">墨尔本</span>
+        </goods>
       </div>
     </div>
+
+
   </div>
 
 </template>
 
 <script>
-  export default {}
+  import goods from "./good.vue";
+  export default {
+    components:{
+      goods
+    }
+  }
 </script>
 
 
@@ -58,4 +83,5 @@
   @import "../../assets/lib/bootstrap/css/bootstrap.css";
   @import "../../assets/css/common.css";
   @import "../../assets/css/recent.css";
+
 </style>
