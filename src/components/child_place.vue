42f69b3c32175ce0c11827c3400343f452b41c40
duanran <904101472@qq.com>
2017-05-28 13:21:43 +0800

    place duanran

---
+++ src/components/child_place.vue
@@ -0,0 +1,273 @@
+<template>
+  <div class="child-place">
+    <div class="navbar navbar-default navbar-fixed-top h-search">
+      <form class="search">
+        <spanc class="glyphicon glyphicon-search"></spanc>
+        <input type="text" placeholder="东京"/>
+        <span>取消</span>
+      </form>
+    </div>
+    <div class="place">
+      <div class="c-p-header">
+        目的地
+        <br><hr />
+      </div>
+      <div class="c-p-xx-list">
+        <div class="c-p-xx-1">
+          <div class="c-p-xx-img">
+            <img src="../assets/img/child-place/c-p-picture.jpg">
+          </div>
+          <div class="c-p-xx-js">
+            <div>
+              东京
+            </div>
+            <div>
+              Tokyo
+            </div>
+            <div>
+              日本
+            </div>
+            <div>
+            <span class="c-p-xx-start">
+
+            </span>
+            <span class="c-p-xx-gs">
+              城市
+            </span>
+            </div>
+            <div>
+              80252人去过
+            </div>
+          </div>
+          <br><hr>
+        </div>
+        <div class="c-p-xx-1">
+          <div class="c-p-xx-img">
+            <img src="../assets/img/child-place/c-p-pic2.jpg">
+          </div>
+          <div class="c-p-xx-js">
+            <div>
+              东京迪斯尼度假区
+            </div>
+            <div>
+              Tokyo Disney Resort
+            </div>
+            <div>
+              日本
+            </div>
+            <div>
+            <span class="c-p-xx-start">
+              <img src="../assets/img/child-place/c-p-start.png">
+            </span>
+            <span class="c-p-xx-gs">
+              其他
+            </span>
+            </div>
+            <div>
+              4135人去过
+            </div>
+          </div>
+          <br><hr>
+        </div>
+        <div class="c-p-xx-1">
+          <div class="c-p-xx-img">
+            <img src="../assets/img/child-place/c-p-picture.jpg">
+          </div>
+          <div class="c-p-xx-js">
+            <div>
+              东京都
+            </div>
+            <div>
+              Tokyo Metropolis
+            </div>
+            <div>
+              日本
+            </div>
+            <div>
+            <span class="c-p-xx-start">
+
+            </span>
+            <span class="c-p-xx-gs">
+              城市
+            </span>
+            </div>
+            <div>
+              1436人去过
+            </div>
+          </div>
+          </a>
+          <br><hr>
+        </div>
+      </div>
+      <div class="p-more">
+        查看更多目的地 >
+      </div>
+    </div>
+    <div class="p-null"></div>
+    <div class="place">
+      <div class="c-p-header">
+        锦囊
+        <br><hr />
+      </div>
+      <div class="c-p-xx-list">
+        <div class="c-p-xx-1">
+          <div class="c-p-xx-img">
+            <img src="../assets/img/child-place/p-jinnang-1.png">
+          </div>
+          <div class="c-p-xx-js">
+            <div>
+              东京
+            </div>
+            <div>
+              亚洲/日本
+            </div>
+            <div>
+              <br>
+            </div>
+            <div>
+              <br>
+            </div>
+            <div>
+              2017-05-17更新
+            </div>
+          </div>
+          <br><hr>
+        </div>
+        <div class="c-p-xx-1">
+          <div class="c-p-xx-img">
+            <img src="../assets/img/child-place/p-jn-food.jpg">
+          </div>
+          <div class="c-p-xx-js">
+            <div>
+              东京美食
+            </div>
+            <div>
+              亚洲/日本
+            </div>
+            <div>
+              <br>
+            </div>
+            <div>
+              <br>
+            </div>
+            <div>
+              2016-09-08更新
+            </div>
+          </div>
+          <br><hr>
+        </div>
+        <div class="c-p-xx-1">
+          <div class="c-p-xx-img">
+            <img src="../assets/img/child-place/p-jn-jd.png">
+          </div>
+          <div class="c-p-xx-js">
+            <div>
+              东京周边
+            </div>
+            <div>
+              亚洲/日本
+            </div>
+            <div>
+              <br>
+            </div>
+            <div>
+              <br>
+            </div>
+            <div>
+              2016-09-29更新
+            </div>
+          </div>
+          <br><hr>
+        </div>
+      </div>
+      <div class="p-more">
+        查看更多锦囊 >
+      </div>
+    </div>
+    <div class="p-null null2"></div>
+    <div class="place">
+      <div class="c-p-header">
+        酒店
+        <div class="c-p-h-icon">
+          <img src="../assets/img/child-place/p-jd-h.png">
+        </div>
+        <br><hr />
+      </div>
+      <div class="c-p-xx-list">
+        <div class="c-p-xx-1">
+          <div class="c-p-xx-img">
+            <img src="../assets/img/child-place/p-jn-jiud.png">
+          </div>
+          <div class="c-p-xx-js">
+            <div class="jd-pos">
+              东京池袋北口2号东京旅馆Toyoko Inn Tokyo Ikebukuro Kita-guchi No.2
+            </div>
+            <div class="jd-dengji">
+              2星级&nbsp;| &nbsp;7.5分 &nbsp;好
+            </div>
+            <div class="jd-city">
+              <span>
+              位于池袋
+            </span>
+              <span class="jd-pri">￥496/晚</span>
+            </div>
+
+          </div>
+          <br><hr>
+        </div>
+        <div class="c-p-xx-1">
+          <div class="c-p-xx-img">
+            <img src="../assets/img/child-place/p-jd2.png">
+          </div>
+          <div class="c-p-xx-js">
+            <div class="jd-pos">
+              东京赤羽站东口一番街东横酒店Toyoko Inn Tokyo Akabane-eki Higashi-guchi Ichiban-gai
+            </div>
+            <div class="jd-dengji">
+              2星级&nbsp;| &nbsp;8.2分 &nbsp;好
+            </div>
+            <div class="jd-city">
+              <span>
+              位于池袋
+            </span>
+            <span class="jd-pri">
+              ￥508/晚
+            </span>
+            </div>
+
+          </div>
+          <br><hr>
+        </div>
+        <div class="c-p-xx-1">
+          <div class="c-p-xx-img">
+            <img src="../assets/img/child-place/p-jd3.png">
+          </div>
+          <div class="c-p-xx-js">
+            <div class="jd-pos">
+              东京东西开赛东横旅馆 Toyoko Inn Toyoko Tozai-Sen Nishi-kasai
+            </div>
+            <div class="jd-dengji">
+              2星级&nbsp;| &nbsp;7.7分 &nbsp;好
+            </div>
+            <div class="jd-city">
+              <span>
+
+              </span>
+              <span class="jd-pri">￥471/晚</span>
+            </div>
+          </div>
+          <br><hr>
+        </div>
+      </div>
+      <div class="p-more">
+        查看更多酒店 >
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+  @import "../assets/css/child_place.css";
+  @import "../assets/lib/bootstrap/css/bootstrap.css";
+  @import "../assets/lib/bootstrap/css/bootstrap-theme.css";
+  @import "../assets/lib/bootstrap/css/bootstrap-theme.min.css";
+</style>
---
+++ src/components/reply.vue
@@ -0,0 +1,134 @@
+<template>
+  <div class="reply-place">
+    <div class="row r-p-header">
+      <span class="col-xs-4">
+        <img src="../assets/img/logo.png" alt=""/>
+      </span>
+      <span class="col-xs-3 r-place">
+        东京
+      </span>
+      <span class="col-xs-2">
+        <span class="glyphicon glyphicon-search"></span>
+        <span class="glyphicon glyphicon-plus"></span>
+      </span>
+    </div>
+    <div class="r-p-body-img">
+      <img src="../assets/img/child-place/c-p-picture.jpg">
+    </div>
+    <div class="r-p-shoucang">
+      <span class="glyphicon glyphicon-heart-empty"></span>
+      <span>我收藏的东京目的地</span>
+      <br><hr>
+    </div>
+    <div class="jingdian">
+      <div class="r-jd-header">
+        <span class="glyphicon glyphicon-thumbs-up"></span>
+        <span>不容错过</span>
+      </div>
+      <div class="col-sm-3 jingdian1">
+        <img src="../assets/img/reply/jingdian1.jpg">
+        <div class="jd-name">
+          浅草寺
+        </div>
+        <div class="r-jd-dengji">
+          <img src="../assets/img/reply/jd-dengji.png">
+          <span>8.3分</span>
+        </div>
+      </div>
+      <div class="col-sm-3 jingdian1">
+        <img src="../assets/img/child-place/c-p-picture.jpg">
+        <div class="jd-name">
+          东京塔
+        </div>
+        <div class="r-jd-dengji">
+          <img src="../assets/img/reply/jd-dengji.png">
+          <span>8.9分</span>
+        </div>
+      </div>
+      <div class="col-sm-3 jingdian1">
+        <img src="../assets/img/reply/jingdian2.jpg">
+        <div class="jd-name">
+          涩谷区
+        </div>
+        <div class="r-jd-dengji">
+          <img src="../assets/img/reply/jd-dengji.png">
+          <span>8.5分</span>
+        </div>
+      </div>
+      <div class="col-sm-3 jingdian1">
+        <img src="../assets/img/reply/jingdian4.jpg">
+        <div class="jd-name">
+          新宿区歌舞伎
+        </div>
+        <div class="r-jd-dengji">
+          <img src="../assets/img/reply/jd-dengji.png">
+          <span>8.1分</span>
+        </div>
+      </div>
+      <br>
+      <hr>
+    </div>
+    <div class="r-p-youji">
+      <div class="r-yj-header">
+        <span class="glyphicon glyphicon-pencil"></span>
+        <span>东京游记推荐</span>
+      </div>
+      <div class="r-yj-body">
+        <div class="r-yj-1">
+          <div class="yj-1-container">
+            【Fiorelady】跟着日剧游东京、京都、关西（1日东京人-东京寺庙静心/浴衣穿越-奈良梅花鹿-大阪买买买）
+          </div>
+          <div class="yj-1-xs">
+            <span>yinyuyinyu</span>
+            <span>|</span>
+            <span>2017-5-27</span>
+            <span>|</span>
+            <span>201人喜欢</span>
+            <span class="glyphicon glyphicon-heart"></span>
+            <span class="yj-pinglun">评论</span>
+          </div>
+          <br><hr>
+        </div>
+        <div class="r-yj-1">
+          <div class="yj-1-container">
+            【Fiorelady】跟着日剧游东京、京都、关西（1日东京人-东京寺庙静心/浴衣穿越-奈良梅花鹿-大阪买买买）
+          </div>
+          <div class="yj-1-xs">
+            <span>yinyuyinyu</span>
+            <span>|</span>
+            <span>2017-5-27</span>
+            <span>|</span>
+            <span>201人喜欢</span>
+            <span class="glyphicon glyphicon-heart"></span>
+            <span class="yj-pinglun">评论</span>
+          </div>
+          <br><hr>
+        </div>
+        <div class="r-yj-1">
+          <div class="yj-1-container">
+            【Fiorelady】跟着日剧游东京、京都、关西（1日东京人-东京寺庙静心/浴衣穿越-奈良梅花鹿-大阪买买买）
+          </div>
+          <div class="yj-1-xs">
+            <span>yinyuyinyu</span>
+            <span>|</span>
+            <span>2017-5-27</span>
+            <span>|</span>
+            <span>201人喜欢</span>
+            <span class="glyphicon glyphicon-heart"></span>
+            <span class="yj-pinglun">评论</span>
+          </div>
+          <br><hr>
+        </div>
+        <div class="yj-more">
+          更多东京游记
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+  @import "../assets/css/reply.css";
+  @import "../assets/lib/bootstrap/css/bootstrap.css";
+  @import "../assets/lib/bootstrap/css/bootstrap-theme.css";
+  @import "../assets/lib/bootstrap/css/bootstrap-theme.min.css";
+</style>
