5362e876cf17a534fda09537d26e0d1fe8815255
admin <2799367148@qq.com>
2017-06-05 23:11:41 +0800

    denglu

---
+++ src/components/tel_login.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="container" style=" height:17.8rem;width: 10rem;background:#40c895;" scoped>
+    <div class="row" style="height:2rem;" scoped>
+      <div class="col-xs-5" style="height: 100%;;width: 4rem;" scoped>
+        <div class="row" style="height: 100% ;" scoped>
+          <router-link to="/user" style="text-decoration:none;color:#f5f7f2;" scoped>
+          <div class="glyphicon glyphicon-arrow-left col-xs-3" aria-hidden="true" style="color:white;height:100%;padding: 0;
+              line-height:2rem;font-size:40px;text-align:center;" scoped>
+          </div>
+          </router-link>
+          <div class="col-xs-9" style="color:white;font-size:40px;text-align:center;height: 100%;padding: 0;line-height: 2rem;" scoped>
+            账号密码
+          </div>
+        </div>
+      </div>
+      <router-link to="/register" style="text-decoration:none;color:#f5f7f2;" scoped>
+      <div class="col-xs-3 col-xs-offset-4" style="height: 100%;" scoped>
+        <div style="height:100%;font-size:30px;color:white;text-align:center;" scoped>
+          <button style="border:1px solid white;display:inline-block;background:#40c895;width: 2rem;height:1rem;text-align: center;line-height: 1rem;margin-top: 0.5rem;" scoped>注册</button>
+        </div>
+      </div>
+        </router-link>
+    </div>
+    <div class="row" style="height: 2rem;color: white;font-size: 30px;" scoped>
+      <div style="text-align: center;line-height: 2rem;" scoped>
+        登录
+      </div>
+    </div>
+    <div style="height: 7rem;" scoped>
+      <div style="border-bottom:2px solid white;height: 1.5rem;text-align:center;" scoped>
+        <input value="输入手机号" style="background:#40c895;border:none;text-align:center;font-size:30px;color:white;line-height:1.5rem;opacity:0.8;" scoped/>
+      </div>
+      <div style="border-bottom:2px solid white;height: 1.5rem;text-align:center;" scoped>
+        <input value="输入密码" style="background:#40c895;border:none;text-align:center;font-size:30px;color:white;line-height:1.5rem;text-align:center;opacity:0.8;" scoped/>
+      </div>
+      <div style="height:1rem;font-size:30px;color:white;line-height:1rem;text-align:center;" scoped>忘记密码？</div>
+      <div style="height:2rem;text-align:center;" scoped>
+        <button style="height:1rem;width: 1.5rem;color:#40c895;font-size:30px;" scoped>登录</button>
+      </div>
+      <router-link to="/login">
+        <div  style="height:1rem;color:white;text-align:center;font-size:30px;" scoped>邮箱/用户名登录</div>
+      </router-link>
+    </div>
+    <div>
+      <div style="height:1rem;color:white;text-align:center;font-size:30px;margin-top:3rem;">第三方账号登录</div>
+      <div class="logo" style="height:2rem;">
+        <div class="weixin col-xs-4" style="text-align: center">
+          <img src="../assets/img/weixin.jpg" style="height:1rem;width: 1rem;">
+        </div>
+        <div class="weibo col-xs-4" style="text-align: center">
+          <img src="../assets/img/weibo.jpg"  style="height:1rem;width: 1rem;">
+        </div>
+        <div class="qq col-xs-4" style="text-align: center">
+          <img src="../assets/img/qq.jpg"  style="height:1rem;width: 1rem;">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+
+  }
+</script>
+
+
+<style src="../assets/css/common.css" scoped>
+</style>
+<style src="../assets/lib/bootstrap/css/bootstrap.css" scoped>
+
+</style>
