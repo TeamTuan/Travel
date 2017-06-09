33eb687884df182a439f900b6ed093f7742502ed
郑佳举 <997238194@qq.com>
2017-06-09 14:09:12 +0800

    游记

--- src/components/add_notes.vue
+++ src/components/add_notes.vue
@@ -3,19 +3,20 @@
   <div  class="row header"  style="height:1.5rem;background:#439865;width:10rem;margin: 0;">
     <div class="col-xs-1"><router-link to="/travel_notes"><span class="glyphicon glyphicon-arrow-left"  style="font-size:30px;" aria-hidden="true"></span></router-link></div>
     <div class="col-xs-10"><span>我的游记</span></div>
-    <div class="col-xs-1"><span class="glyphicon glyphicon-ok"  style="font-size:30px;" aria-hidden="true"></span></div>
+
+    <div class="col-xs-1"><span @click="save_blog" class="glyphicon glyphicon-ok"  style="font-size:30px;" aria-hidden="true"></span></div>
   </div>
   <div class="row my_row" style="margin: 0;">
     <div class="col-xs-6">
       <h2>标题</h2>
-      <input type="text" value="" name="" size="45" style="width:8rem; height:1rem; border-radius: 0.75rem; font-size:33px;" >
+      <input type="text" value="" name="title" size="45" style="width:8rem; height:1rem; border-radius: 0.75rem; font-size:33px;" v-model="blog_title">
     </div>
   </div>
 
   <div class="row my_row1" style="margin: 0;">
     <div class="col-xs-6">
       <h2>我的游记</h2>
-      <textarea type="text" value="" name="" size="45" style="width:7rem; height:7rem; border-radius: 0.3rem; font-size:33px;" ></textarea>
+      <textarea type="text" value="" name="content" size="45" style="width:7rem; height:7rem; border-radius: 0.3rem; font-size:33px;" v-model="blog_content"></textarea>
     </div>
   </div>
 
@@ -25,8 +26,12 @@
   </div>
     </div>
   <div class="img_border">
-    <div class="my_footer"> <span class="glyphicon glyphicon-plus"
-                                               aria-hidden="true"></span></div>
+    <div class="my_footer">
+      <span class="glyphicon glyphicon-plus" aria-hidden="true">
+        <input type="file" style="font-size: 1px" name="img">
+        <img src="" alt="">
+      </span>
+     </div>
   </div>
 </div>
 
@@ -34,13 +39,50 @@
 </template>
 
 <script>
+  import Axios from "axios";
   export default {
     data:function () {
       return {
-        timer:""
+        timer:"",
+        blog_title:"",
+        blog_content:"",
+        blog_img:"",
+//        login_id:0
       }
     },
+//    mounted:function(){
+//      if(document.cookie){
+//        var arr = document.cookie.split((";")[1]);
+//        var new_arr = arr.split("=")[1];
+//        this.login_id = Number(new_arr[1]);
+//      }
+//
+//    },
+
     methods:{
+      save_blog:function(){
+        var blog_title = this.blog_title;
+        var blog_content = this.blog_content;
+        var blog_img = this.blog_img;
+
+        var _this=this;
+        Axios.get("http://localhost:3000/save_blog",{
+          params:{
+//            id:_this.login_id,
+            blog_title:blog_title,
+            blog_content:blog_content,
+            blog_img:blog_img,
+
+
+
+          }
+        }).then(function(res){
+          console.log(res.data);
+        })
+
+
+      }
+
 
     }
   }
--- src/components/travel-notes.vue
+++ src/components/travel-notes.vue
@@ -1,62 +1,66 @@
 <template>
+<div>
+  <div class="travel-notes"  >
+    <div class="a_header">
+      <router-link to="/user/null/null" style="color: white;"><div><span class="arrows"><span class="glyphicon glyphicon-arrow-left" aria-hidden="true"></span></span></div></router-link>
+
+      <div class="col-xs-8"><h2>我的游记</h2></div>
+    </div>
+    <div class="my_nav" v-for="travel in travellist">
+
+      <div class="row my_row" style="margin: 0;">
+
+        <div class="col-xs-9">
+          <img src="../assets/img/xianggang.jpg">
+          <h2>{{travel.title}}</h2>
+        </div>
+        <div class="col-xs-2"><router-link to="/add_notes"><span class="glyphicon glyphicon-pencil"
+                                                                 aria-hidden="true"></span></router-link></a></div>
+        <div class="col-xs-1"><a href=""><span class="glyphicon glyphicon-remove"
+                                               aria-hidden="true"></span></a></div>
+      </div>
 
 
-        <div class="travel-notes">
-            <div class="a_header">
-              <router-link to="/user/null/null" style="color: white;"><div><span class="arrows"><span class="glyphicon glyphicon-arrow-left" aria-hidden="true"></span></span></div></router-link>
 
-                    <div class="col-xs-8"><h2>我的游记</h2></div>
-            </div>
-            <div class="my_nav" >
-
-                <div class="row my_row" style="margin: 0;">
-
-                  <div class="col-xs-9">
-                    <img src="../assets/img/xianggang.jpg">
-                    <h2>香港</h2>
-                  </div>
-                  <div class="col-xs-2"><router-link to="/add_notes"><span class="glyphicon glyphicon-pencil"
-                                                                              aria-hidden="true"></span></router-link></a></div>
-                  <div class="col-xs-1"><a href=""><span class="glyphicon glyphicon-remove"
-                                                                              aria-hidden="true"></span></a></div>
-                </div>
-
-                <div class="row my_row" style="margin: 0;">
-                  <div class="col-xs-9">
-                    <img src="../assets/img/harbin.jpg">
-                    <h2>哈尔滨</h2>
-                  </div>
-                  <div class="col-xs-2"><router-link to="/add_notes"><span class="glyphicon glyphicon-pencil"
-                                                                              aria-hidden="true"></span></router-link></a></div>
-                  <div class="col-xs-1"><a href=""><span class="glyphicon glyphicon-remove"
-                                                         aria-hidden="true"></span></a></div></div>
-
-                </div>
-            <div class="footer_add">
-                <router-link to="/add_notes">
+    </div>
+    <div class="footer_add">
+      <router-link to="/add_notes">
                   <span class="glyphicon glyphicon-plus"
                         aria-hidden="true"></span>
-                </router-link>
+      </router-link>
 
 
 
 
-            </div>
-        </div>
+    </div>
+  </div>
+</div>
+
+
 
 </template>
 <script>
+  import Axios from "axios";
+  export default {
+    data:function () {
+      return {
+        travellist:[],
+        login_id:0
+      }
+    },
 
-    export default {
-        data:function () {
-            return {
-                timer:""
-            }
-        },
-        methods:{
 
-        }
+        created:function () {
+         var _this=this;
+
+      Axios.get("http://localhost:3000/travel_notes")
+        .then(function (res) {
+          _this.travellist=JSON.parse(res.data);
+//          console.log(JSON.parse(res.data));
+        });
+
     }
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
